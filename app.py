9c102ca92e6804b0b354165096adf9f012036ec6
Sthummar <84076740+Sthummar@users.noreply.github.com>
2024-12-02 08:30:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from sklearn.linear_model import LogisticRegression
 import pickle
 
 # Load the trained model and scaler
-with open('logistic_regression_model.pkl', 'rb') as model_file:
+with open('logistic_regression_model.pkl1', 'rb') as model_file:
     model = pickle.load(model_file)
 
-with open('scaler.pkl', 'rb') as scaler_file:
+with open('scaler1.pkl', 'rb') as scaler_file:
     scaler = pickle.load(scaler_file)
 
 # Streamlit app
